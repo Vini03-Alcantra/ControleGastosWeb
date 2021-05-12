8a94ac28d6a20116f785299140b796457c32b6ab
Vinicus <viniciusalcantarabarros@gmail.com>
2021-05-11 23:42:02 -0300

    Atualizando displays de valores]

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const transactionsUl = document.querySelector("#transactions")
+const incomeDisplay = document.querySelector("#money-plus")
+const expenseDisplay = document.querySelector("#money-minus")
+const balanceDisplay = document.querySelector("#balance")
+
 
 const dummyTransactions = [
     {id: 1, name: "Auxílio Transporte", amount: 80},
@@ -20,8 +24,20 @@ const addTransactionIntoDOM = transaction => {
     transactionsUl.prepend(li)
 }
 
+const updateBalanceValues = () => {
+    const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
+    const total = transactionsAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+    const income = transactionsAmounts.filter(value => value > 0).reduce((accumulator, value) => accumulator+value, 0).toFixed(2)
+    const expense = transactionsAmounts.filter(value => value < 0).reduce((accumulator, value) => accumulator+value)
+    
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+}
+
 const init = () => {
     dummyTransactions.forEach(addTransactionIntoDOM)
+    updateBalanceValues()
 }
 
 init()
